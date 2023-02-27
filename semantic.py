a94fb4e343c1a0b12a1ef65185b259e0012b29ff
Irina.Anisimova <Irina.Anisimova.WorldSkills@gmail.com>
2023-02-27 20:47:15 +0000

    first commit

---
+++ semantic.py
@@ -0,0 +1,21 @@
+import spacy
+sentence_to_compare = "Why is my cat on the car"
+sentences = ["where did my dog go",
+"Hello, there is my car",
+"I\'ve lost my car in my car",
+"I\'d like my boat back",
+"I will name my dog Diana"]
+nlp = spacy.load('en_core_web_sm')
+model_sentence = nlp(sentence_to_compare)
+for sentence in sentences:
+    similarity = nlp(sentence).similarity(model_sentence)
+print("sm model " + sentence + " - ", similarity)
+
+
+nlp = spacy.load('en_core_web_md')
+model_sentence = nlp(sentence_to_compare)
+for sentence in sentences:
+    similarity = nlp(sentence).similarity(model_sentence)
+print("md model " +sentence + " - ", similarity)
+
+quit()
